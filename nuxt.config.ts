d2e0312f22949e5e181b0c39252aba1f64b45167
Sayam Zafar <72272560+sayamrao@users.noreply.github.com>
2026-09-24 21:44:54 +0500

    Disable homepage prerendering for live product data

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -72,13 +72,9 @@ export default defineNuxtConfig({
 
   nitro: {
     compressPublicAssets: true,
-    prerender: {
-      routes: ['/'],
-    },
   },
 
   routeRules: {
-    '/': { prerender: true },
     '/products/**': { swr: 3600 },
     '/dashboard/**': { ssr: false },
     '/seller/**': { ssr: false },
